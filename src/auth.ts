9fff3e0bc6afeeaa7d59f2abf7273f329f8b50f7
Lucas Soranzo <lucasdeonsoranzo@gmail.com>
2022-12-21 12:57:49 -0300

    feat: add jwks support

--- src/auth.ts
+++ src/auth.ts
@@ -1,35 +1,18 @@
-import axios from "axios";
 import * as jose from "jose";
-
-let publicKeys: Record<string, string> | undefined;
-
-async function loadPublicKeys() {
-  const domain = process.env.TBX_DOMAIN || "trials.testbox.com";
-  const keys = await axios.get<Record<string, string>>(
-    `https://${domain}/.well-known/keys`
-  );
-  publicKeys = keys.data;
-}
 
 export async function verifyAuthenticationToken(
   token: string,
   trialId: string,
   audienceClaim: string
 ): Promise<boolean> {
-  if (!publicKeys) {
-    await loadPublicKeys();
-  }
-  const kid = jose.decodeProtectedHeader(token).kid;
-  if (!kid || !publicKeys) {
-    return false;
-  }
-  const publicKey = publicKeys[kid];
-  if (!publicKey) {
-    return false;
-  }
   try {
-    const importedKey = await jose.importSPKI(publicKey, "RS256");
-    const results = await jose.jwtVerify(token, importedKey, {
+    const publicKey = jose.createRemoteJWKSet(
+      new URL(
+        process.env.TBX_JWKS_URL ||
+          "https://partners-well-known.s3.us-west-2.amazonaws.com/.well-known/jwks.json"
+      )
+    );
+    const results = await jose.jwtVerify(token, publicKey, {
       audience: audienceClaim,
     });
     return results.payload.trial_id == trialId;
---
+++ test/helpers.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import nock from "nock";
+
+export const nockJwks = async () => {
+  await new Promise<void>((resolve) => {
+    const filePath = path.dirname(__filename) + "/fixtures/jwks";
+    fs.readFile(filePath, (err, data) => {
+      const contents = data.toString("utf-8");
+      nock(
+        process.env.TBX_JWKS_URL ||
+          "https://partners-well-known.s3.us-west-2.amazonaws.com"
+      )
+        .get("/.well-known/jwks.json")
+        .reply(200, contents, {
+          "Content-Type": "application/json",
+        });
+      resolve();
+    });
+  });
+};
